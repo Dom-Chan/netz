0b76721743d5609d9c35d93b82e588f6978bac74
dominic-glyph <dominic@glyph.com.ph>
2023-01-11 14:01:04 +0800

    tested api endpoints

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+const handler = async (req, res) => {
+  console.log("fetching...");
+
+  try {
+    const resp = await fetch(
+      "https://netzwelt-devtest.azurewebsites.net/Account/SignIn",
+      {
+        method: req.method,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req.body)
+      }
+    );
+
+    const data = await resp.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default handler;
